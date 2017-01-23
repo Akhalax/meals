122b0caa6c3ad24d6976f1c76b05f95d1f8a2012
User <user@elenas-mac-mini.local>
2017-01-23 17:04:25 +0200

    Initial Commit

---
+++ Meal.swift
@@ -0,0 +1,24 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by User on 1/19/17.
+//  Copyright © 2017 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class Meal: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
